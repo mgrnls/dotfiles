8b583e51cf21f8102a22b11d83e889eadfbd73bb
Matthew Greenlees <m.greenlees@protonmail.com>
2020-06-21 14:25:37 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.vim ~/.vim
+ln -s -f ~/dotfiles/.scripts ~/.scripts
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/.inputrc ~/.inputrcf
+ln -s -f ~/dotfiles/.templates ~/.templates
