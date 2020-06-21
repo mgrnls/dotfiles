b784ce77c9e34b63130414c71f8d29b99d8a3b38
Matthew Greenlees <m.greenlees@protonmail.com>
2020-06-21 16:46:07 +0200

    Add verbose flags

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# Sym link dot files.
-ln -s -f ~/dotfiles/.vimrc ~/.vimrc
-ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s -f ~/dotfiles/.inputrc ~/.inputrcf
+echo "Linking dot files."
+echo "=================="
+ln -sv -f ~/dotfiles/.vimrc ~/.vimrc
+ln -sv -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sv -f ~/dotfiles/.inputrc ~/.inputrcf
+echo 
 
-# Sym link directories.
-#ln -s -f ~/dotfiles/.vim ~/.vim
-#ln -s -f ~/dotfiles/.templates ~/.templates
-#ln -s -f ~/dotfiles/.scripts ~/.scripts
+echo "Deleting old dot directories."
+echo "============================="
+rm -v ~/.vim
+rm -v ~/.templates
+rm -v ~/.scripts
+echo
+
+echo "Linking new dot directories."
+echo "============================"
+ln -sv -f ~/dotfiles/.vim ~/.vim
+ln -sv -f ~/dotfiles/.templates ~/.templates
+ln -sv -f ~/dotfiles/.scripts ~/.scripts
