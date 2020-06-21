91279a58d817e0de209a50e75f4a3f606c202e8b
Matthew Greenlees <m.greenlees@protonmail.com>
2020-06-21 16:19:23 +0200

    Add old inputrc, recovered from homer, comment out symlinking directories

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Sym link dot files.
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
-ln -s -f ~/dotfiles/.vim ~/.vim
-ln -s -f ~/dotfiles/.scripts ~/.scripts
 ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s -f ~/dotfiles/.inputrc ~/.inputrcf
-ln -s -f ~/dotfiles/.templates ~/.templates
+
+# Sym link directories.
+#ln -s -f ~/dotfiles/.vim ~/.vim
+#ln -s -f ~/dotfiles/.templates ~/.templates
+#ln -s -f ~/dotfiles/.scripts ~/.scripts
