45fa128ddcf883b2528990542f7722217d529f98
Matthew Greenlees <m.greenlees@protonmail.com>
2020-07-04 19:20:34 +0200

    Revert install method to rsync thing

--- install.sh
+++ install.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-echo "Linking dot files."
-echo "=================="
-ln -sv -f ~/dotfiles/.vimrc ~/.vimrc
-ln -sv -f ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sv -f ~/dotfiles/.inputrc ~/.inputrcf
-echo 
-
-echo "Deleting old dot directories."
-echo "============================="
-rm -v ~/.vim
-rm -v ~/.templates
-rm -v ~/.scripts
-echo
-
-echo "Linking new dot directories."
-echo "============================"
-ln -sv -f ~/dotfiles/.vim ~/.vim
-ln -sv -f ~/dotfiles/.templates ~/.templates
-ln -sv -f ~/dotfiles/.scripts ~/.scripts
+rsync . ~/ -a --exclude=.git --exclude=README.md
